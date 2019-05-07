e58f22f6fa8c1b44ef1dbbf3a874ef0337271242
Sarang N <sarang98divakaran@gmail.com>
2019-05-07 15:52:29 +0530

    Initial Commit :tada:

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'genreml-vue-frontend'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
