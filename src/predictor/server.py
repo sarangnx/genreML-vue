336cb001577ddfb7d353702e4600395f42392d19
Sarang <sarang98divakaran@gmail.com>
2019-06-12 21:45:26 +0530

    Communicate with JSON

--- src/predictor/server.py
+++ src/predictor/server.py
@@ -1,48 +1,6 @@
-# import socketio
-
-# # create a Socket.IO server
-# sio = socketio.Server()
-# app = socketio.WSGIApp(sio)
-
-# @sio.on('my custom event')
-# def another_event(sid, data):
-#     pass
-
-
-# from aiohttp import web
-# import socketio
-
-# sio = socketio.AsyncServer()
-# app = web.Application()
-# sio.attach(app)
-# @sio.event
-# async def message(sid, message):
-#     print("Socket ID: " , sid)
-#     print(message)
-
-
-# web.run_app(app,port=8000)
-
-# import eventlet
-# import socketio
-
-# sio = socketio.Server()
-
-# @sio.event
-# def connect(sid, environ):
-#     print('connect ', sid)
-
-# @sio.event
-# def my_message(sid, data):
-#     print('message ', data)
-
-# @sio.event
-# def disconnect(sid):
-#     print('disconnect ', sid)
-
-# eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
-
 import zmq
+import json
+import predict
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
@@ -50,8 +8,15 @@ socket.bind("tcp://*:5555")
 
 while True:
     #  Wait for next request from client
-    message = socket.recv()
-    print("Received request: %s" % message)
+    query = socket.recv()
+    
+    queryObject = json.loads(query)
 
+    if(queryObject.mode == "single"):
+        socket.send_string("SINGLE")
+    else:
+        socket.send_string("BATCH")
+
+    # print(json.dumps(query))
     #  Send reply back to client
-    socket.send(b"World")
+    # socket.send_string(json.dumps(query))
